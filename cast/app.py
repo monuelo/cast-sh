e6ef9370f8d562f99a5ed9e670eea2dcb25515d0
Hericles <hericles@lsd.ufcg.edu.br>
2019-08-27 13:57:26 -0300

    update socket endpoints

--- cast/app.py
+++ cast/app.py
@@ -11,20 +11,12 @@ import struct
 import fcntl
 import shlex
 
-
 __version__ = "0.0.1"
 
 app = Flask(__name__, template_folder=".", static_folder=".", static_url_path="")
-# app.config["SECRET_KEY"] = "secret!"
 app.config["fd"] = None
 app.config["child_pid"] = None
 socketio = SocketIO(app)
-
-
-def set_winsize(fd, row, col, xpix=0, ypix=0):
-    winsize = struct.pack("HHHH", row, col, xpix, ypix)
-    fcntl.ioctl(fd, termios.TIOCSWINSZ, winsize)
-
 
 def read_and_forward_pty_output():
     max_read_bytes = 1024 * 20
@@ -35,7 +27,7 @@ def read_and_forward_pty_output():
             (data_ready, _, _) = select.select([app.config["fd"]], [], [], timeout_sec)
             if data_ready:
                 output = os.read(app.config["fd"], max_read_bytes).decode()
-                socketio.emit("pty-output", {"output": output}, namespace="/pty")
+                socketio.emit("client-output", {"output": output}, namespace="/cast")
 
 
 @app.route("/")
@@ -43,43 +35,22 @@ def index():
     return render_template("index.html")
 
 
-@socketio.on("pty-input", namespace="/pty")
-def pty_input(data):
-    """write to the child pty. The pty sees this as if you are typing in a real
-    terminal.
-    """
+@socketio.on("client-input", namespace="/cast")
+def client_input(data):
     if app.config["fd"]:
-        # print("writing to ptd: %s" % data["input"])
         os.write(app.config["fd"], data["input"].encode())
 
-
-@socketio.on("resize", namespace="/pty")
-def resize(data):
-    if app.config["fd"]:
-        set_winsize(app.config["fd"], data["rows"], data["cols"])
-
-
-@socketio.on("connect", namespace="/pty")
+@socketio.on("connect", namespace="/cast")
 def connect():
-    """new client connected"""
-
     if app.config["child_pid"]:
-        # already started child process, don't start another
         return
 
-    # create child process attached to a pty we can read from and write to
     (child_pid, fd) = pty.fork()
     if child_pid == 0:
-        # this is the child process fork.
-        # anything printed here will show up in the pty, including the output
-        # of this subprocess
         subprocess.run(app.config["cmd"])
     else:
-        # this is the parent process fork.
-        # store child fd and pid
         app.config["fd"] = fd
         app.config["child_pid"] = child_pid
-        set_winsize(fd, 50, 50)
         cmd = " ".join(shlex.quote(c) for c in app.config["cmd"])
         print("child pid is", child_pid)
         print(
@@ -93,7 +64,7 @@ def connect():
 def main():
     parser = argparse.ArgumentParser(
         description=(
-            "A fully functional terminal in your browser. "
+            "A fully functional terminal in your browser."
             "https://github.com/hericlesme/cast.sh"
         ),
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
