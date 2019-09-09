6e80832b773f4406c918af34274c652311d8eed9
Héricles Emanuel <hericles@lsd.ufcg.edu.br>
2019-09-09 17:03:23 -0300

    fix tabs link

--- cast/app.js
+++ cast/app.js
@@ -1,10 +1,8 @@
+const $ = document.querySelector.bind(document);
 var currentSession;
-
-Terminal.applyAddon(fullscreen);
-Terminal.applyAddon(fit);
-Terminal.applyAddon(webLinks);
-Terminal.applyAddon(search);
-
+var tabs = []
+var sessions = [];
+var TID = 0;
 const MAX_LINES = 9999999;
 
 const generateSSID = () => (
@@ -12,45 +10,42 @@ const generateSSID = () => (
   + Math.random().toString(36).substring(2, 15)
 );
 
-const termSetup = (term, ssid) => {
-  currentSession.term.open(document.getElementById(ssid));
-  currentSession.term.fit();
-  currentSession.term.setOption("scrollback", MAX_LINES);
-  currentSession.term.writeln("Welcome to cast.sh! - https://github.com/hericlesme/cast-sh - Press [Enter] to Start");
-  getTabBySSID(ssid).session = currentSession;
-  console.log(term);
-  console.log(currentSession.ssid);
-}
+Terminal.applyAddon(fullscreen);
+Terminal.applyAddon(fit);
+Terminal.applyAddon(webLinks);
+Terminal.applyAddon(search);
 
-const Cast = (ssid, tid, term) => {
-  this.tid = tid;
-  this.term = term;
-  this.ssid = ssid;
+
+
+
+const termSetup = (term, ssid) => {
+
+  term.open(document.getElementById(ssid));
+  term.fit();
+  term.setOption("scrollback", MAX_LINES);
+  term.writeln("Welcome to cast.sh! - https://github.com/hericlesme/cast-sh - Press [Enter] to Start");
 
   term.on("key", (key, ev) => {
-    socket.emit("client-input", { input: key, session_id: sessionId });
+    socket.emit("client-input", { input: key, session_id: ssid });
   });
 
-  currentSession = { ssid: ssid, term: term };
   getTabBySSID(ssid).session = currentSession;
-  termSetup(term, ssid);
-
-  return this.ssid;
 }
 
-var tabs = []
-var TID = 0;
+const Cast = (ssid, tid) => {
 
-// const term = new Terminal({
-//   cursorBlink: true,
-//   macOptionIsMeta: true,
-//   scrollback: true
-// });
+  term = new Terminal({
+    cursorBlink: true,
+    macOptionIsMeta: true,
+    scrollback: true
+  });
 
-// termSetup(term);
+  currentSession = { ssid: ssid, term: term, tid: tid };
+  termSetup(term, ssid);
+  return currentSession;
+}
 
-
-const openSession = (tid) => {
+const resetStyle = (tid) => {
   let i, tabcontent, tablinks;
 
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -69,13 +64,21 @@ const openSession = (tid) => {
   let tablink = document.getElementById(tid);
   tablink.style.backgroundColor = "greenyellow";
   tablink.style.color = "black";
+  console.log("oii:  " + getTabByTID(tid).ssid);
+}
 
+
+
+const openSession = (tid) => {
+  resetStyle(tid);
   let tab = getTabByTID(tid);
-  console.log(tab);
-  currentSession = tab.session;
   console.log(tab.ssid);
+  // currentSession = tab.session;
   document.getElementById(tab.ssid).style.display = "block";
 }
+
+
+/*** HTML Elements ***/
 
 const newTab = (ssid) => {
   let tab = document.createElement("div");
@@ -87,41 +90,50 @@ const newTab = (ssid) => {
   return tab;
 }
 
-const createTab = () => {
-  let ssid = generateSSID();
+const appendTab = (ssid) => {
+  let header = $(".header");
+  let element = $("#create-tab");
 
-  let termContainer = document.getElementById("term-container");
-  let header = document.getElementsByClassName("header")[0];
-  let element = document.getElementById("create-tab");
   let tab = newTab(ssid);
   tab.addEventListener('click', (e) => {
     console.log(e.target.id);
     openSession(e.target.id);
   })
-  header.insertBefore(tab, element);
 
+  header.insertBefore(tab, element);
+}
+
+const appendContent = (ssid) => {
+  let termContainer = document.getElementById("term-container");
   let content = document.createElement("div");
   content.className = "tabcontent";
   content.id = ssid;
   termContainer.appendChild(content);
+}
 
 
-  Cast(ssid, ++TID, new Terminal({
-    cursorBlink: true,
-    macOptionIsMeta: true,
-    scrollback: true
-  }));
+/*** Create Tab ***/
+
+const createTab = () => {
+  let ssid = generateSSID();
+
+  appendTab(ssid);
+  appendContent(ssid);
+
+  let term = Cast(ssid, ++TID);
+
+  sessions.push(term);
+  console.log(sessions);
 
   openSession(TID - 1);
 }
-
 
 
 /*** Utils ***/
 
 const getTabByTID = (tid) => tabs.find(t => t.tid == tid);
 
-const getTabBySSID = (ssid) => tabs.find(t => t.ssid = ssid);
+const getTabBySSID = (ssid) => tabs.find(t => t.ssid == ssid);
 
 createTab();
 
@@ -129,7 +141,7 @@ createTab();
 /*** Socket Settings ***/
 
 
-const socket = io.connect("/cast", { query: `session_id=${sessionId}` });
+const socket = io.connect("/cast", { query: `session_id=${currentSession.ssid}` });
 const status = document.getElementById("status");
 
 socket.on("client-output", (data) => {
