9779e63f09a066d343a93a7483e0c8644632d2a2
Vlad Doster <mvdoster@gmail.com>
2019-11-05 21:43:31 -0500

    Unit tests (#73)
    * Adding coveralls
    
    * updated travis
    
    * Update .coveralls
    
    * added pytest-cov for coveralls
    
    * I think I was right that the deps were just cached which made it seem that we didnt need them in the .travis.yml. I readded them in a oneliner.
    
    * Added comment to help explain
    
    * Adding coveralls hook
    
    * added badge
    
    * Added back deps I removed
    
    * Added black formatrting check and readme badge
    
    * Added black dep to travis pip install
    
    * Moved python requirments up to 3.6 so all versions of python support black
    
    * Changed back the file permissions
    
    * Last references to bad permissions
    
    * Reverted permissions back to 644
    
    * Updated README.md to reflect supported python version bumps
    
    * Moved python requirments up to 3.6 so all versions of python support black
    
    * Updated unit tests to increase coverage
    
    * Changed back permissions
    
    * Commenting out selenium references until we figure out if we need it in the long run.
    
    * Fixed wekezurg dep version

--- tests/test_cast.py
+++ tests/test_cast.py
@@ -1,50 +1,30 @@
-import pytest
-from time import sleep
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import unittest
+
+from cast.app import app
 
 
-@pytest.fixture(scope="class")
-def chrome_driver_init(request):
-    c_options = webdriver.ChromeOptions()
-    # All the arguments added for chromium to work on selenium
-    c_options.add_argument("--no-sandbox")  # This make Chromium reachable
-    c_options.add_argument("--headless")
-    c_options.add_argument("--disable-gpu")
-    c_options.add_argument("--no-default-browser-check")  # Overrides default choices
-    c_options.add_argument("--no-first-run")
-    c_options.add_argument("--disable-default-apps")
-    chrome_driver = webdriver.Chrome(
-        chrome_options=c_options, executable_path="/usr/bin/chromedriver"
-    )
-    request.cls.driver = chrome_driver
-    yield
-    chrome_driver.close()
+class CastShTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
 
+    @classmethod
+    def tearDownClass(cls):
+        pass
 
-@pytest.mark.usefixtures("chrome_driver_init")
-class BasicChromeTest:
-    pass
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
 
+        # propagate the exceptions to the test client
+        self.app.testing = True
 
-# Test for correct html
-class TestCast(BasicChromeTest):
-    def test_serving_app_html(self):
-        self.driver.get("http://127.0.0.1:5000")
-        sleep(2.5)  # TODO:Should be implicit wait
-        assert self.driver.title == "cast.sh"
+    def tearDown(self):
+        pass
 
-    def test_empty_session_log_download(self):
-        self.driver.get("http://127.0.0.1:5000")
-        tabs = self.driver.find_elements(By.CLASS_NAME, "tab")
-        assert len(tabs) == 1
-        close_tabs = self.driver.find_elements(By.CLASS_NAME, "close")
-        assert len(close_tabs) == 1
-        close_tabs[0].click()
-        tabs = self.driver.find_elements(By.CLASS_NAME, "tab")
-        assert tabs[0].text == "[closed] tab 1"
-        log_btn = self.driver.find_element(By.CLASS_NAME, "log")
-        log_btn.click()
-        sleep(2)
-        no_log_avlble = self.driver.find_element(By.CLASS_NAME, "notyf__message")
-        assert no_log_avlble.text == "No log available for download."
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get("/")
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
