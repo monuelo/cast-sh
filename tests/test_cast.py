a683dc49e5ad7ae8cbbc4b2566faa2405c936700
Vlad Doster <mvdoster@gmail.com>
2019-11-06 05:00:13 -0500

    Unit tests (#74)
    * Adding coveralls
    
    * updated travis
    
    * Update .coveralls
    
    * added pytest-cov for coveralls
    
    * I think I was right that the deps were just cached which made it seem that we didnt need them in the .travis.yml. I readded them in a oneliner.
    
    * Added comment to help explain
    
    * Adding coveralls hook
    
    * added badge
    
    * Added back deps I removed
    
    * Added black formatrting check and readme badge
    
    * Added black dep to travis pip install
    
    * Moved python requirments up to 3.6 so all versions of python support black
    
    * Changed back the file permissions
    
    * Last references to bad permissions
    
    * Reverted permissions back to 644
    
    * Updated README.md to reflect supported python version bumps
    
    * Moved python requirments up to 3.6 so all versions of python support black
    
    * Updated unit tests to increase coverage
    
    * Changed back permissions
    
    * Commenting out selenium references until we figure out if we need it in the long run.
    
    * Fixed wekezurg dep version
    
    * Added more unit tests
    
    * Reformatted test
    
    * Merging changes
    
    * Merging changes
    
    * Cleaned up cruft code
    
    * Few more crufty imports sneaking around
    
    * Updated comments to test cli options

--- tests/test_cast.py
+++ tests/test_cast.py
@@ -1,17 +1,9 @@
 import unittest
 
-from cast.app import app
+from cast.app import app, create_parser
 
 
 class CastShTests(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        pass
-
-    @classmethod
-    def tearDownClass(cls):
-        pass
-
     def setUp(self):
         # creates a test client
         self.app = app.test_client()
@@ -28,3 +20,32 @@ class CastShTests(unittest.TestCase):
         result = self.app.get("/")
         # assert the status code of the response
         self.assertEqual(result.status_code, 200)
+
+
+class CastShCLIOptionsTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        parser = create_parser()
+        cls.parser = parser
+
+    def test_help(self):
+        """
+        User passes --help, should fail with SystemExit because it printed to stdout
+        """
+        with self.assertRaises(SystemExit):
+            self.parser.parse_args(["--help"])
+
+    def test_version(self):
+        """
+        User passes --version, should register in argparse as True
+        """
+        args = self.parser.parse_args(["--version"])
+        self.assertTrue(args.version)
+
+    def test_bad_version(self):
+        """
+        User passes --help only, version flag should not be true
+        """
+        with self.assertRaises(SystemExit):
+            args = self.parser.parse_args(["--help"])
+            self.assertFalse(args.version)
