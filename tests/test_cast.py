eb7158d2546cb55a08951b3b84f85cdd240057f3
Vlad Doster <mvdoster@gmail.com>
2019-11-06 14:14:47 -0500

    More tests

--- tests/test_cast.py
+++ tests/test_cast.py
@@ -6,23 +6,18 @@ from cast.app import app, create_parser
 
 class CastShRouteTests(unittest.TestCase):
     def setUp(self):
-        # creates a test client
         self.app = app.test_client()
         # propagate the exceptions to the test client
         self.app.testing = True
 
     def test_home_status_code(self):
-        # sends HTTP GET request to the application
-        # on the specified path
+        """ Tests / """
         result = self.app.get("/")
-        # assert the status code of the response
         self.assertEqual(result.status_code, 200)
 
     def test_download_404_code(self):
-        # sends HTTP GET request to the application
-        # on the specified path
+        """ Tests /download/ """
         result = self.app.get("/download/")
-        # assert the status code of the response
         self.assertEqual(result.status_code, 404)
         result = self.app.get("/download/{}".format(randint(1, 100000)))
         self.assertEqual(result.status_code, 404)
@@ -34,12 +29,42 @@ class CastShCLIOptionsTests(unittest.TestCase):
         parser = create_parser()
         cls.parser = parser
 
+    def test_cmd_args(self):
+        args = self.parser.parse_args([])
+        self.assertEqual(args.cmd_args, "")
+        args = self.parser.parse_args(["--cmd-args", "ls"])
+        self.assertEqual(args.cmd_args, "ls")
+
+    def test_command(self):
+        """
+        User passes --help, should fail with SystemExit because it printed to stdout
+        """
+        args = self.parser.parse_args([])
+        self.assertEqual(args.command, "bash")
+
+    def test_debug(self):
+        """
+        User passes --help, should fail with SystemExit because it printed to stdout
+        """
+        args = self.parser.parse_args(["--debug"])
+        self.assertTrue(args.debug)
+
     def test_help(self):
         """
         User passes --help, should fail with SystemExit because it printed to stdout
         """
         with self.assertRaises(SystemExit):
             self.parser.parse_args(["--help"])
+
+    def test_ports(self):
+        """
+        User passes --help, should fail with SystemExit because it printed to stdout
+        """
+        customer_port = str(randint(6000, 8000))
+        args = self.parser.parse_args(["-p", customer_port])
+        self.assertEqual(args.port, customer_port)
+        args = self.parser.parse_args(["--port", customer_port])
+        self.assertEqual(args.port, customer_port)
 
     def test_version(self):
         """
