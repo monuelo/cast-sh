064b2b32f9e220caddfc74860c127b3e6447fc9b
Héricles Emanuel <hericles.me@gmail.com>
2019-11-05 22:14:52 -0300

    start simple login

--- cast/app.py
+++ cast/app.py
@@ -1,26 +1,47 @@
 #!/usr/bin/env python3
 import argparse
-from flask import Flask, render_template, request, send_from_directory
+from flask import (
+    Flask,
+    render_template,
+    request,
+    send_from_directory,
+    redirect,
+    url_for,
+)
 import flask_socketio
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity, get_jwt_claims
+)
 from werkzeug.exceptions import BadRequest
-
+import json
 from .logger import Logging
 import pty
 import os
 import subprocess
 import select
 import shlex
+import random
+import string
 
 __version__ = "0.0.1"
 
-app = Flask(__name__, template_folder=".", static_folder=".", static_url_path="")
+app = Flask(__name__, template_folder=".",
+            static_folder=".", static_url_path="")
 app.config["fd"] = None
+app.config["logged"] = False
+app.config["private"] = False
 app.config["child_pid"] = None
 app.config["current_session"] = None
 app.config["sessions"] = {}
 app.config["log_file"] = r"log_data/"
 socketio = flask_socketio.SocketIO(app)
 
+
+def random_string(string_length=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(string_length))
 
 def read_and_forward_pty_output(session_id):
     max_read_bytes = 1024 * 20
@@ -35,21 +56,51 @@ def read_and_forward_pty_output(session_id):
 
             if file_desc:
                 timeout_sec = 0
-                (data_ready, _, _) = select.select([file_desc], [], [], timeout_sec)
+                (data_ready, _, _) = select.select(
+                    [file_desc], [], [], timeout_sec)
                 if data_ready:
                     output = os.read(file_desc, max_read_bytes).decode()
                     socketio.emit(
                         "client-output",
-                        {"output": output, "ssid": app.config["current_session"]},
+                        {"output": output,
+                            "ssid": app.config["current_session"]},
                         namespace="/cast",
                     )
 
 
+def setup_session():
+    
+    app.config['JWT_SECRET_KEY'] = random_string()
+    jwt = JWTManager(app)
+
+
+    passwd = os.getenv('CAST_PASSWORD')
+    if passwd is not None:
+        app.config["private"] = True
+        app.config["passwd"] = passwd
+
+@jwt_required
 @app.route("/")
 def index():
+    if app.config["private"] and not app.config["logged"]:
+        return redirect(url_for("login"))
     log = Logging(app.config["current_session"])
     log.make_log_folder()
     return render_template("index.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    elif request.method == 'POST':
+        data = request.json
+        if data["password"] == app.config["passwd"]:
+            access_token = create_access_token(identity=os.getenv('JOB_ID'))
+            app.config['logged'] = True
+            return json.dumps({"access_token": access_token}), 200
+        else:
+            return json.dumps(request.get_json()), 401
 
 
 @socketio.on("new-session", namespace="/cast")
@@ -186,9 +237,12 @@ def main():
         ),
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
-    parser.add_argument("-p", "--port", default=5000, help="port to run server on")
-    parser.add_argument("--debug", action="store_true", help="debug the server")
-    parser.add_argument("--version", action="store_true", help="print version and exit")
+    parser.add_argument("-p", "--port", default=5000,
+                        help="port to run server on")
+    parser.add_argument("--debug", action="store_true",
+                        help="debug the server")
+    parser.add_argument("--version", action="store_true",
+                        help="print version and exit")
     parser.add_argument(
         "--command", default="bash", help="Command to run in the terminal"
     )
@@ -201,6 +255,8 @@ def main():
     if args.version:
         print(__version__)
         exit(0)
+
+    setup_session()
     print("serving on http://0.0.0.0:{}".format(args.port))
     app.config["cmd"] = [args.command] + shlex.split(args.cmd_args)
     socketio.run(app, host="0.0.0.0", debug=args.debug, port=args.port)
