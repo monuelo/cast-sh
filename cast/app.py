5690cb792701fbaf062a6b11f36bd08a6939699e
Héricles Emanuel <hericles.me@gmail.com>
2021-06-21 09:38:47 -0300

    Fix formatting with black (#113)

--- cast/app.py
+++ cast/app.py
@@ -25,7 +25,7 @@ app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
 
 
 def random_string(string_length=10):
-    """Generate a random string of fixed length """
+    """Generate a random string of fixed length"""
     letters = string.ascii_lowercase
     return "".join(random.choice(letters) for i in range(string_length))
 
